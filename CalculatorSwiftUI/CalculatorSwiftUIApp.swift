3e636078bfb67194a43c80a5608b57027ab39227
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-08-14 23:14:00 +0900

    Initial Commit

---
+++ CalculatorSwiftUI/CalculatorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorSwiftUIApp.swift
+//  CalculatorSwiftUI
+//
+//  Created by joe on 2023/08/14.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
